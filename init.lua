16d42c38e471f6b2c750d36feb8f2af0a719cf70
tomiock <ockier1@gmail.com>
2023-12-13 22:28:01 +0100

    change theme to moonfly from catppuccin

--- init.lua
+++ init.lua
@@ -191,7 +191,9 @@ require('lazy').setup({
 
   -- THEME
   --{'navarasu/onedark.nvim', priority = 1000, config = function() vim.cmd.colorscheme 'onedark' end,},
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 };
+  --{ "catppuccin/nvim", name = "catppuccin", priority = 1000 };
+  
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 
   {
     -- Set lualine as statusline
@@ -200,7 +202,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'moonfly',
         component_separators = '|',
         section_separators = '',
       },
@@ -286,56 +288,56 @@ require('lazy').setup({
 -- NOTE: You can change these options as you wish!
 --
 -- THEMEEEE
-
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-})
+--  require("catppuccin").setup({
+--    flavour = "mocha", -- latte, frappe, macchiato, mocha
+--    background = { -- :h background
+--        light = "latte",
+--        dark = "mocha",
+--    },
+--    transparent_background = true, -- disables setting the background color.
+--    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--    dim_inactive = {
+--        enabled = false, -- dims the background color of inactive window
+--        shade = "dark",
+--        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+--    },
+--    no_italic = false, -- Force no italic
+--    no_bold = false, -- Force no bold
+--    no_underline = false, -- Force no underline
+--    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--        comments = { "italic" }, -- Change the style of comments
+--        conditionals = { "italic" },
+--        loops = {},
+--        functions = {},
+--        keywords = {},
+--        strings = {},
+--        variables = {},
+--        numbers = {},
+--        booleans = {},
+--        properties = {},
+--        types = {},
+--        operators = {},
+--    },
+--    color_overrides = {},
+--    custom_highlights = {},
+--    integrations = {
+--        cmp = true,
+--        gitsigns = true,
+--        nvimtree = true,
+--        treesitter = true,
+--        notify = false,
+--        mini = {
+--            enabled = true,
+--            indentscope_color = "",
+--        },
+--        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+--    },
+--  })
 
 -- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+vim.g.moonflyTransparent = true
+vim.cmd.colorscheme "moonfly"
 
 -- Set highlight on search
 vim.o.hlsearch = false
