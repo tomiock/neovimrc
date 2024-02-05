d4d0041cb90d74ee8736be62395460b19725b98e
tomiock <ockier1@gmail.com>
2024-02-05 09:57:16 +0100

    colors on neovim separeted file
    for future issues:
     - load the colorscheme on this file, DO NOT put it into the lazy/init.lua file
     - it appear that you cannot combine different colorscheme. the config
       on ThePrimagen is just one config that is overwritten into another
       one

---
+++ lua/tomiock/lazy/colors.lua
@@ -0,0 +1,11 @@
+
+return {
+    {
+	'bluz71/vim-moonfly-colors',
+    	name = 'moonfly',
+    	priority = 1000,
+    	config = function()
+    	    vim.cmd.colorscheme 'moonfly'
+    	end,
+    },
+}
