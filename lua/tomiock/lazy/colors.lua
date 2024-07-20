ac862a4ae16a9d389db0a39bdc76c0a581012971
tomiock <ockier1@gmail.com>
2024-07-20 15:19:03 +0200

    feat: new theme (cyberdream)

--- lua/tomiock/lazy/colors.lua
+++ lua/tomiock/lazy/colors.lua
@@ -1,11 +1,43 @@
-
 return {
-    {
-	'bluz71/vim-moonfly-colors',
-    	name = 'moonfly',
-    	priority = 1000,
-    	config = function()
-    	    vim.cmd.colorscheme 'moonfly'
-    	end,
-    },
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+
+		config = function()
+			require("cyberdream").setup({
+
+				transparent = true,
+				terminal_colors = true,
+				borderless_telescope = true,
+
+				theme = {
+					highlights = {
+						-- Highlight groups to override, adding new groups is also possible
+						-- See `:h highlight-groups` for a list of highlight groups or run `:hi` to see all groups and their current values
+
+						-- Example:
+						Comment = { fg = "#696969", bg = "NONE", italic = false },
+
+						-- Complete list can be found in `lua/cyberdream/theme.lua`
+					},
+
+					-- Override default colors
+					colors = {
+						green = "#00ff00",
+					},
+
+					-- Disable or enable colorscheme extensions
+					extensions = {
+						telescope = true,
+						notify = true,
+						mini = true,
+					},
+				},
+			})
+
+			vim.cmd("colorscheme cyberdream")
+		end,
+
+	},
 }
