c4cc587d0221a4b1f72445f78edd828174fd14a2
tomiock <ockier1@gmail.com>
2024-08-06 16:20:12 +0200

    a little lint and new search files

--- lua/tomiock/lazy/telescope.lua
+++ lua/tomiock/lazy/telescope.lua
@@ -61,7 +61,10 @@ return {
 	pcall(require('telescope').load_extension, 'fzf')
 
 	vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recent files' })
-	vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[<space>] Find buffers' })
+	vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+	vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+	vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[<space>] Find buffers' })
+
 	vim.keymap.set('n', '<leader>/', function()
 	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
 		winblend = 10,
@@ -78,7 +81,6 @@ return {
 	vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
 	vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 	vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-	vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 	vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 	vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 	vim.keymap.set('n', '<leader>sG', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
