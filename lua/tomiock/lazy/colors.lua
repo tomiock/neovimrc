e8b6ccd5c92bce6ea6df0452d2356ece7c49e8d2
tomiock <ockier1@gmail.com>
2025-03-26 15:39:39 +0100

    update harpoon 2 and colors

--- lua/tomiock/lazy/colors.lua
+++ lua/tomiock/lazy/colors.lua
@@ -11,34 +11,32 @@ return {
 				terminal_colors = true,
 				borderless_telescope = false,
 
-				theme = {
-					-- Override a highlight group entirely using the color palette
-					overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
-						-- Example:
-						return {
-							Keyword = { fg = "#ff73ff", bg = "NONE", bold = false },
-							Number = { fg = "#b0d7ff", bg = "NONE", bold = false },
-							Boolean = { fg = "#b0d7ff", bg = "NONE", bold = false },
-							PreProc = { fg = "#b0d7ff", bg = "NONE", bold = false },
-							parameter = { fg = "#ff00ff", bg = "NONE", bold = false },
+				-- Override a highlight group entirely using the color palette
+				overrides = function(colors) -- NOTE: This function nullifies the `highlights` option
+					-- Example:
+					return {
+						Keyword = { fg = "#ff73ff", bg = "NONE", bold = false },
+						Number = { fg = "#b0d7ff", bg = "NONE", bold = false },
+						Boolean = { fg = "#b0d7ff", bg = "NONE", bold = false },
+						PreProc = { fg = "#b0d7ff", bg = "NONE", bold = false },
+						parameter = { fg = "#ff00ff", bg = "NONE", bold = false },
 
-							DiagnosticVirtualTextBad = { fg = "#a4ad3d", bg = "NONE", bold = false },
-							DiagnosticVirtualTextWarn = { fg = "#a4ad3d", bg = "NONE", bold = false },
-							DiagnosticVirtualTextHint = { fg = "#368e96", bg = "NONE", bold = false },
-						}
-					end,
+						DiagnosticVirtualTextBad = { fg = "#a4ad3d", bg = "NONE", bold = false },
+						DiagnosticVirtualTextWarn = { fg = "#a4ad3d", bg = "NONE", bold = false },
+						DiagnosticVirtualTextHint = { fg = "#368e96", bg = "NONE", bold = false },
+					}
+				end,
 
-					-- Override default colors
-					colors = {
-						green = "#61ffca",
-					},
+				-- Override default colors
+				colors = {
+					green = "#61ffca",
+				},
 
-					-- Disable or enable colorscheme extensions
-					extensions = {
-						telescope = true,
-						notify = true,
-						mini = true,
-					},
+				-- Disable or enable colorscheme extensions
+				extensions = {
+					telescope = true,
+					notify = true,
+					mini = true,
 				},
 			})
 
--- lua/tomiock/lazy/harpoon.lua
+++ lua/tomiock/lazy/harpoon.lua
@@ -1,39 +1,50 @@
 return {
 	"theprimeagen/harpoon",
-	 -- require("telescope").load_extension("harpoon"),
+	branch = "harpoon2",
+	-- Ensure you have plenary installed, as harpoon depends on it
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("harpoon").setup({
-		global_settings = { tabline = true },
-		menu = {
-			width = vim.api.nvim_win_get_width(0) - 20,
+		local harpoon = require("harpoon")
+
+		-- Call setup with your original options
+		harpoon:setup({
+			global_settings = { tabline = true },
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 20,
 			}
 		})
 
-local mark = require('harpoon.mark')
-local ui = require('harpoon.ui')
+		-- Define keymaps using the new API
 
-vim.keymap.set('n', '<leader>ha', mark.add_file, {desc = '[A]add file to harpoon list'})
-vim.keymap.set('n', '<leader>hr', mark.remove_empty_tail, {desc = 'dont know what it does'})
-vim.keymap.set('n', '<leader>hn', ui.nav_next, {desc = '[N]ext file in harpoon list'})
-vim.keymap.set('n', '<leader>hp', ui.nav_prev, {desc = '[P]revious file in harpoon list'})
+		-- Add current file to the harpoon list
+		vim.keymap.set('n', '<leader>ha', function() harpoon:list():add() end, {desc = '[A]dd file to harpoon list'})
 
-vim.keymap.set('n', '<C-s>', ui.toggle_quick_menu)
+		-- Navigate to the next file in the harpoon list
+		vim.keymap.set('n', '<leader>hn', function() harpoon:list():next() end, {desc = '[N]ext file in harpoon list'})
 
-vim.keymap.set('n', '<C-h>', function () ui.nav_file(1) end)
-vim.keymap.set('n', '<C-j>', function () ui.nav_file(2) end)
-vim.keymap.set('n', '<C-k>', function () ui.nav_file(3) end)
-vim.keymap.set('n', '<C-l>', function () ui.nav_file(4) end)
+		-- Navigate to the previous file in the harpoon list
+		vim.keymap.set('n', '<leader>hp', function() harpoon:list():prev() end, {desc = '[P]revious file in harpoon list'})
 
--- keymaps with ALT
-vim.keymap.set('n', '<M-1>', function () ui.nav_file(1) end)
-vim.keymap.set('n', '<M-2>', function () ui.nav_file(2) end)
-vim.keymap.set('n', '<M-3>', function () ui.nav_file(3) end)
-vim.keymap.set('n', '<M-4>', function () ui.nav_file(4) end)
-vim.keymap.set('n', '<M-5>', function () ui.nav_file(5) end)
-vim.keymap.set('n', '<M-6>', function () ui.nav_file(6) end)
-vim.keymap.set('n', '<M-7>', function () ui.nav_file(7) end)
-vim.keymap.set('n', '<M-8>', function () ui.nav_file(8) end)
-vim.keymap.set('n', '<M-9>', function () ui.nav_file(9) end)
-vim.keymap.set('n', '<M-0>', function () ui.nav_file(10) end)
+		-- Toggle the quick menu (using your original keybind <C-s>)
+		vim.keymap.set('n', '<C-s>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = 'Toggle Harpoon quick menu'})
+
+		-- Navigate to specific files by index using C-<key>
+		vim.keymap.set('n', '<C-h>', function () harpoon:list():select(1) end, {desc = 'Harpoon select file 1'})
+		vim.keymap.set('n', '<C-j>', function () harpoon:list():select(2) end, {desc = 'Harpoon select file 2'})
+		vim.keymap.set('n', '<C-k>', function () harpoon:list():select(3) end, {desc = 'Harpoon select file 3'})
+		vim.keymap.set('n', '<C-l>', function () harpoon:list():select(4) end, {desc = 'Harpoon select file 4'})
+
+		-- Navigate to specific files by index using M-<number>
+		vim.keymap.set('n', '<M-1>', function () harpoon:list():select(1) end, {desc = 'Harpoon select file 1'})
+		vim.keymap.set('n', '<M-2>', function () harpoon:list():select(2) end, {desc = 'Harpoon select file 2'})
+		vim.keymap.set('n', '<M-3>', function () harpoon:list():select(3) end, {desc = 'Harpoon select file 3'})
+		vim.keymap.set('n', '<M-4>', function () harpoon:list():select(4) end, {desc = 'Harpoon select file 4'})
+		vim.keymap.set('n', '<M-5>', function () harpoon:list():select(5) end, {desc = 'Harpoon select file 5'})
+		vim.keymap.set('n', '<M-6>', function () harpoon:list():select(6) end, {desc = 'Harpoon select file 6'})
+		vim.keymap.set('n', '<M-7>', function () harpoon:list():select(7) end, {desc = 'Harpoon select file 7'})
+		vim.keymap.set('n', '<M-8>', function () harpoon:list():select(8) end, {desc = 'Harpoon select file 8'})
+		vim.keymap.set('n', '<M-9>', function () harpoon:list():select(9) end, {desc = 'Harpoon select file 9'})
+		-- Note: Index 10 might be intended here for <M-0>
+		vim.keymap.set('n', '<M-0>', function () harpoon:list():select(10) end, {desc = 'Harpoon select file 10'})
 	end
 }
